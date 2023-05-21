462810ed54d2a8e81e8238e5117bdd29fcdc5b0e
Andres Infante <a.infante@uc.cl>
2023-05-21 00:06:40 -0400

    w

--- main.py
+++ main.py
@@ -37,33 +37,28 @@ w = m.addVar(A, vtype=GRB.BINARY, name='w_a')
 m.update()
 # Coloque aca sus restricciones
 # (1): 
-m.addConstrs((quicksum(u[a, c] for a in A) <= m for c in Ca), name='R1')
+m.addConstrs((quicksum(u[a, c] for a in A) <= m[c] for c in Ca), name='R1')
 
 # (2): 
-m.addConstrs(y[a][c] >= k * p for a in A for c in Ca, name='R2')
+m.addConstrs((y[a, c] >= k[a] * p[c] for a in A for c in Ca), name='R2')
 
 # (3): 
-m.addConstrs((x[i, j] <= y[j] for i in I for j in J), name='R3')
+m.addConstr((quicksum(y[a, c] for a in A for c in Ca) <= Cmax), name='R3')
 
 # (4): 
-m.addConstrs((x[i, j] >= y[j] - quicksum(y[k] for k in J if t[i, k] < t[i, j])
-             for i in I for j in J), name='R4')
+m.addConstr((quicksum(n[c] * z[c] for c in Ca) <= f * Cmax), name='R4')
 
 # (5): 
-m.addConstrs((v[i] <= quicksum(y[j] for j in J if t[i, j] <= alpha)
-             for i in I), name='R5')
+m.addConstrs((x[a, b] * s[a] >= h[b] for a in A for b in A if a != b), name='R5')
 # (6): 
-m.addConstrs((w[i] <= quicksum(y[j] for j in J if t[i, j] <= beta)
-             for i in I), name='R6')
+m.addConstrs((abs(n[c] - 1) * J + quicksum(y[a, c] * d + w[a] * Co[a] for a in A) <= D[c] for c in Ca), name='R6')
 # (7): 
-m.addConstr(quicksum(v[i] for i in I)/len(I) == 0.85 + z1, name='R7')
+m.addConstrs((y[a, c] >= u[a, c] for a in A for c in Ca), name='R7')
 # (8): 
-denominador = quicksum(c[i] for i in I)
-numerador = quicksum((w[i]*c[i]) for i in I)
-m.addConstr(numerador == denominador * (0.9 + z2), name='R8')
+m.addConstrs((w[a] * k[a] * N <= k[a] for a in A), name='R8')
 
 m.update()
-objetivo = quicksum(y[a][c] + (n[c] * z[c]) - u[a][c] for a in A for c in Ca)
+objetivo = quicksum((y[a, c] + (n[c] * z[c]) - u[a, c]) for a in A for c in Ca)
 m.setObjective(objetivo, GRB.MINIMIZE)  # Colocar la FO
 m.optimize()
 
