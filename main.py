4fc2ad5c036433e31ad260a494b748c76a5e641a
Andres Infante <a.infante@uc.cl>
2023-05-21 22:11:58 -0400

    hasta la R12

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Ca = range(10)  # Casas
 A = range(8)  # Actividades
 T = range(1, 25)  # Horas del día
 # PARAMS
+MM = 10^10  # mm >> 0
 k = [randint(3, 20) for a in A]
 b = [randint(1, 10) for a in A]
 m = [randint(0, 1000) for c in Ca]
@@ -33,32 +34,50 @@ v = model.addVars(Ca, vtype=GRB.CONTINUOUS, name='v_c')
 q = model.addVars(A, Ca, T, vtype=GRB.CONTINUOUS, name='q_act')
 model.update()
 # Coloque aca sus restricciones
-# (1) La cantidad de agua reciclada debe ser menor a la capacidad del estanque en cada momento t:
+# (1) La cantidad de agua reciclada debe ser menor a la capacidad del estanque
+# en cada momento t:
 model.addConstrs((M[t, c] <= m[c] for t in T for c in Ca), name='R1')
 
 # (2)La cantidad de agua almacenada en el estanque en el periodo t (inventario):
-model.addConstrs((M[t, c] == M[t-1, c] + quicksum(x[a, c, t] * k[a] for a in A)
-                 - quicksum(r[a, c, t] * k[a] for a in A) for t in T for c in Ca if t >= 2), name='R2')
+model.addConstrs((M[t, c] == M[t-1, c] + quicksum(x[a, c, t]*k[a] for a in A)
+                 - quicksum(r[a, c, t]*k[a] for a in A) for t in T for c in
+                 Ca if t >= 2), name='R2')
 
 # (3)Cantidad inicial del estanque: 
 model.addConstrs((M[1, c] == 0 for c in Ca), name='R3')
 
-# (4) No se puede realizar la actividad a con agua reciclada del estanque si es que este no posee suficiente agua:
-model.addConstrs((M[t, c] >= r[a, c, t] * k[a] for a in A for c in Ca for t in T), name='R4')
+# (4) No se puede realizar la actividad a con agua reciclada del estanque si
+# es que este no posee suficiente agua:
+model.addConstrs((M[t, c] >= r[a, c, t]*k[a] for a in A for c in Ca for t in T), name='R4')
 
 # (5) El agua solo se puede reciclar si tiene un grado alfa de calidad:
 model.addConstr((x[a, c, t] <= s[a] for a in A for c in Ca for t in T), name='R5')
 
-# (6)Solo se puede utilizar el agua del estanque si la actividad no requiere una calidad sobre alfa para ser realizada: 
+# (6) Solo se puede utilizar el agua del estanque si la actividad no requiere
+# una calidad sobre alfa para ser realizada: 
 model.addConstrs((r[a, c, t] <= h[a] for a in A for c in Ca for t in T), name='R6')
 
-# (7)No se puede reciclar el agua de la actividad a si esta no se realiza:
+# (7) No se puede reciclar el agua de la actividad a si esta no se realiza:
 model.addConstrs((x[a, c, t] <= g[a, c, t] for a in A for c in Ca for t in T), name='R7')
+
 # (8):
 #m.addConstrs((), name='R8')
 
+# (9) La adquisición de nuevas tecnologías para llevar a cabo una actividad debe
+# traer una reducción del agua consumida en comparación a la tecnología anterior:
+model.addConstrs((w[a, c]*k[a]*N[a] == y[a, c, t] + q[a, c, t] for a in A for c in Ca for t in T), name='R9')
+
+# (10) Si se decide realizar mantenimiento, las fugas disminuyen en un factor f.
+model.addConstrs((n[c]*z[c]*f + (1 - n[c])*z[c] == v[c] for c in Ca ), name='R10')
+
+# (11) Las actividades se realizan la cantidad mínima de veces necesarias:
+model.addConstrs((quicksum(g[a, c, t] for t in T) >= b[a] for a in A for c in Ca), name='R11')
+
+# (12) Las actividades se realizan la cantidad mínima de veces necesarias:
+model.addConstrs((MM*r[a, c, t] >= q[a, c, t] for a in A for c in Ca for t in T), name='R12')
+
 model.update()
-objetivo = quicksum((y[a, c] + v[c] - x[a, c, t] * k[a]) for a in A for c in Ca for t in T)
+objetivo = quicksum((y[a, c] + v[c] - x[a, c, t]*k[a]) for a in A for c in Ca for t in T)
 model.setObjective(objetivo, GRB.MINIMIZE)  # Colocar la FO
 model.optimize()
 
