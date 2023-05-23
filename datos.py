d243db604138caedef26f2a173341ff13f8aee71
Andres Infante <a.infante@uc.cl>
2023-05-22 20:09:06 -0400

    s

--- datos.py
+++ datos.py
@@ -10,13 +10,14 @@ A = range(10)  # 10 Actividades
 T = range(1, 25)  # 24 Horas del día
 
 # PARAMS
+# En cientos de litros
 MM = 10 ** 10  # mm >> 0
 k = [0.10, 1.5, 0.08, 0.08, 0.08, 0.02, 0.08, 0.15, 1.5, 4]
 b = [8, 4, 8, 8, 1, 4, 1, 8, 1, 1]
 m = [randint(1, 10) for c in Ca]
 z = [uniform(0, 0.3) for c in Ca]  
 D = [randint(5000, 6000) for c in Ca]
-d = 0.015
+d = 0.015 
 J = 43000
 f = 0.7
 Co = [randint(333, 3333) for a in A]
